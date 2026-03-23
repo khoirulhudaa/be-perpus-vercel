f6bdd47712df6f7d2acec658e1615087173d192b
khoirulhudaadev <muhammadkhoirulhuda111@gmail.com>
2026-03-23 13:02:02 +0700

    fix

--- routes/serialRoutes.js
+++ routes/serialRoutes.js
@@ -7,9 +7,9 @@ router.post('/subscriptions', serCtrl.createSubscription);
 router.get('/subscriptions', cache(120), serCtrl.getAllSubscriptions);
 router.post('/generate-issues', serCtrl.generateIssues);
 router.put('/receive-issue/:id', serCtrl.receiveIssue);
-// TAMBAHKAN ROUTE BARU INI:
 router.get('/issues', serCtrl.getIssuesBySubscription);
 router.put('/issues/:id', serCtrl.updateIssue);
+
 router.delete('/issues/:id', serCtrl.deleteIssue);
     
 module.exports = router;
